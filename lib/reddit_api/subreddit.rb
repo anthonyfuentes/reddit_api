af4f00bdac32e14e335c7ba348f229af64ba1a38
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-02-14 18:16:02 -0500

    Implement RedditApi::Subreddit top

--- lib/reddit_api/subreddit.rb
+++ lib/reddit_api/subreddit.rb
@@ -2,5 +2,39 @@
 module RedditApi
   class Subreddit
 
+    def initialize
+      @client = RedditApi::Client.new
+    end
+
+    def top(count)
+      subreddits_data = top_data(count)
+      cleanse_all_data(subreddits_data)
+    end
+
+    private
+    attr_reader :client
+
+    def top_data(count)
+      url = "https://oauth.reddit.com/subreddits/popular.json"
+      query = { limit: count }
+      response = client.get(url, query)
+      response["data"]["children"]
+    end
+
+    def cleanse_all_data(subreddits_data)
+      subreddits_data.map! do |subreddit_data|
+        cleanse_data(subreddit_data)
+      end
+    end
+
+    def cleanse_data(subreddit_data)
+      {
+        subscriber_count: subreddit_data["data"]["subscribers"],
+        url: subreddit_data["data"]["url"],
+        name: subreddit_data["data"]["display_name"],
+        description: subreddit_data["data"]["public_description"]
+      }
+    end
+
   end
 end
--- spec/subreddit_spec.rb
+++ spec/subreddit_spec.rb
@@ -2,4 +2,15 @@ require "spec_helper"
 
 describe RedditApi::Subreddit do
 
+  describe "#top" do
+    it "returns an array of the top n subreddits" do
+      subreddit_api = RedditApi::Subreddit.new
+      count = 5
+
+      result = subreddit_api.top(count)
+
+      expect(result.length).to eq(5)
+    end
+  end
+
 end
