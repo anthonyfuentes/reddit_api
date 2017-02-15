210ffc5fe0e78b85b5bfce32076ef34a70d772df
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-02-15 11:06:04 -0500

    Refactor posts_spec to use Subreddit objects

--- spec/posts_spec.rb
+++ spec/posts_spec.rb
@@ -3,25 +3,25 @@ require "spec_helper"
 describe RedditApi::Posts do
 
   describe "#top" do
-    it "excludes stickied posts that are included by default" do
-      posts_api = RedditApi::Posts.new
-      subreddit = double()
-      allow(subreddit).to receive(:name).and_return("AskReddit")
-
-      result = posts_api.top(subreddit, 5)
-      stickied = result.any? { |p| p["data"]["stickied"] }
-
-      expect(stickied).to be false
-    end
-
     it "returns n top posts of a subreddit" do
       posts_api = RedditApi::Posts.new
-      subreddit = double()
-      allow(subreddit).to receive(:name).and_return("AskReddit")
+      subreddit_params = { "display_name" => "AskReddit" }
+      subreddit = RedditApi::Subreddit.new(subreddit_params)
 
       result = posts_api.top(subreddit, 5)
 
       expect(result.length).to eq(5)
+    end
+
+    it "excludes stickied posts that are included by default" do
+      posts_api = RedditApi::Posts.new
+      subreddit_params = { "display_name" => "AskReddit" }
+      subreddit = RedditApi::Subreddit.new(subreddit_params)
+
+      result = posts_api.top(subreddit, 5)
+
+      stickied = result.any? { |p| p["data"]["stickied"] }
+      expect(stickied).to be false
     end
   end
 
