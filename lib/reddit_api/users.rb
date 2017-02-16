ebaa7eca943b66805562b5244dd4478d795f5b4f
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-02-15 20:44:20 -0500

    Refactor RedditApi::Users
    Uses new RedditApi::Client#get
    Returns user objects

--- lib/reddit_api/users.rb
+++ lib/reddit_api/users.rb
@@ -4,26 +4,59 @@ module RedditApi
 
     def initialize
       @post_api = RedditApi::Posts.new
+      @user_factory = RedditApi::User
     end
 
     def top_posters(subreddit, count)
-      posters = {}
-      while posters.length < count
+      users = {}
+      while users.length < count
         posts = post_api.top(subreddit, count)
-        collect_posters(posts, count, posters)
+        collect_users(posts, count, users)
       end
-      posters.keys
+      users.values
     end
 
     private
-    attr_reader :post_api
+    attr_reader :post_api, :user_factory
 
-    def collect_posters(posts, count, posters)
-      posts.each do |post|
-        posters[post.author] = true
-        break if posters.length == count
+    def collect_users(posts, count, users)
+      usernames = get_usernames(posts, count, users)
+      user_data = format_user_data(usernames)
+      new_users = build_all_users(user_data)
+      aggregate_users(new_users, users)
+    end
+
+    def get_usernames(posts, count, users)
+      usernames = []
+      posts.each_with_index do |post, i|
+        break if (users.length + i) == count
+        usernames << post.author
+      end
+      usernames
+    end
+
+    def format_user_data(usernames)
+      usernames.map! do |username|
+        { "username" => username }
+      end
+    end
+
+    def build_all_users(users_data)
+      users_data.map! do |user_data|
+        build_user(user_data)
+      end
+    end
+
+    def build_user(user_data)
+      user_factory.new(user_data)
+    end
+
+    def aggregate_users(new_users, collected_users)
+      new_users.each do |user|
+        collected_users[user.username] = user
       end
     end
 
   end
 end
+
--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -5,12 +5,33 @@ describe RedditApi::Users do
   describe "#top_posters" do
     it "returns n authors of the top posts from a subreddit" do
       users_api = RedditApi::Users.new
-      subreddit_params = { "display_name" => "AskReddit" }
-      subreddit = RedditApi::Subreddit.new(subreddit_params)
+      count = 5
+      subreddit = RedditApi::Subreddit.new({ "display_name" => "AskReddit" })
+
+      result = users_api.top_posters(subreddit, count)
+
+      expect(result.length).to eq(count)
+    end
+
+    it "returns user objects" do
+      users_api = RedditApi::Users.new
+      subreddit = RedditApi::Subreddit.new({ "display_name" => "AskReddit" })
 
       result = users_api.top_posters(subreddit, 5)
+      all_user_objects = result.all? { |u| u.is_a?(RedditApi::User) }
 
-      expect(result.length).to eq(5)
+      expect(all_user_objects).to be true
+    end
+
+    it "returns unique users" do
+      users_api = RedditApi::Users.new
+      count = 5
+      subreddit = RedditApi::Subreddit.new({ "display_name" => "AskReddit" })
+
+      result = users_api.top_posters(subreddit, count)
+      unique_users = result.uniq { |u| u.username }
+
+      expect(unique_users.length).to eq(count)
     end
   end
 
